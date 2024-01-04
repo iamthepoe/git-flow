eaaa127d63f8dd862ffafea4b880e458a71cc5d7
Luca Poe <iamthepoe>
2024-01-04 11:02:05 -0300

    feature: notification feature

--- index.ts
+++ index.ts
@@ -6,9 +6,16 @@ interface Message{
 
 function sendMessage(message: Message): void{
   const {content, from, to} = message;
-  return console.log(`
+  console.log(`
     ${content} 
     From: ${from}
     To: ${to}
   `);
+
+  sendNotification(to);
+}
+
+function sendNotification(receiver: Message['to']): void{
+  console.log(`Sending notification to ${receiver}...`);
+  setTimeout(()=>{console.log('Done!')}, 1000);
 }
